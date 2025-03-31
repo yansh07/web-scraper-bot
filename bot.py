0abdf00b2eaf9c94ff235c05d0c879c6c73ddf2b
Priyanshu <pksingh69313@gmail.com>
2025-03-31 15:55:53 +0530

    added some additional information

--- bot.py
+++ bot.py
@@ -3,19 +3,22 @@ from telegram import Update
 from telegram.ext import Application, CommandHandler
 import datetime
 
-TELEGRAM_BOT_TOKEN = "YOUR-API-TOKEN-FROM-TELEGRAM"
+TELEGRAM_BOT_TOKEN = "YOUR-API-TOKEN-FROM-TELEGRAM" 
+#search @botfather on telegram, and /start then /newbot to make a new bot, 
+#then you'll be asked to name your bot and then set a username,
+#afterward it will provide you a api code, keep it safe
 
 async def get_weather():
-    today = datetime.date.today()
-    formatted_today = today.strftime('%d-%m-%Y')
-    day_name = today.strftime("%A")
+    today = datetime.date.today() #it will show today's date
+    formatted_today = today.strftime('%d-%m-%Y') #this will show the date in dd:mm:yy format
+    day_name = today.strftime("%A") #this will print day's name
 
     async with async_playwright() as p:
-        browser = await p.firefox.launch(headless=True)
-        page = await browser.new_page()
+        browser = await p.firefox.launch(headless=True) #whole process like opening of browser and searching will be hidden
+        page = await browser.new_page() #opens a browser 
         await page.goto('https://www.accuweather.com/en/in/mussoori/1-196520_1_al/current-weather/1-196520_1_al', timeout=60000)
         temp = await page.locator('div.display-temp').text_content()
-        await browser.close()
+        await browser.close() #closes the browser when the scraping is completed
     
     return f"🌤️ Temperature of Mussoorie on {day_name}, {formatted_today}: {temp}"
 
